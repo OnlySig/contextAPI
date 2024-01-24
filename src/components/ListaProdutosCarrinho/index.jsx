85db761aae8d7f5305e4367d8976c83e4ea3414a
OnlySig <gabriel.castro.pereira11@gmail.com>
2024-01-24 11:20:58 -0300

    removendo propDrilling usando context API, vulgo, componente do react useContext()

--- src/components/ListaProdutosCarrinho/index.jsx
+++ src/components/ListaProdutosCarrinho/index.jsx
@@ -3,12 +3,7 @@ import ItemCarrinhoSuspenso from "@/components/CarrinhoSuspenso/ItemCarrinhoSusp
 import ItemCarrinho from "@/components/ItemCarrinho";
 import { useLocation } from "react-router-dom";
 
-const ListaProdutosCarrinho = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-}) => {
+const ListaProdutosCarrinho = ({ carrinho }) => {
   const location = useLocation();
   return (
     <ul className="list-unstyled">
@@ -16,22 +11,9 @@ const ListaProdutosCarrinho = ({
         <p className="text-center my-5">Não há produtos no carrinho</p>
       ) : (
         carrinho.map((itemCarrinho) => {
-          return location.pathname === "/carrinho" ? (
-            <ItemCarrinho
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
-            />
+          return location.pathname === "/carrinho" ? (<ItemCarrinho key={itemCarrinho.id} itemCarrinho={itemCarrinho} />
           ) : (
-            <ItemCarrinhoSuspenso
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
-            />
+            <ItemCarrinhoSuspenso key={itemCarrinho.id} itemCarrinho={itemCarrinho} />
           );
         })
       )}
